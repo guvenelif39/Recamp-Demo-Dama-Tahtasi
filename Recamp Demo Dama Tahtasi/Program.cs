fcf4e29587bd29252e6a0b28774511a0ea4a889d
guvenelif39 <76227099+guvenelif39@users.noreply.github.com>
2021-01-25 16:08:13 +0300

    Add project files.

---
+++ Recamp Demo Dama Tahtasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recamp_Demo_Dama_Tahtasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
